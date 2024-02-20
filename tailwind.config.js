d0fd15620f975b8b93414bd4107379104c4ba0d0
Desmond-Fon <desmondfon@yahoo.com>
2024-02-20 01:34:22 +0100

    added libraries needed

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+      },
+      colors: {
+        primary: "#282928",
+        secondary: "#239463",
+        tertiary: "#24211f",
+      },
+    },
+  },
+  plugins: [],
+};
