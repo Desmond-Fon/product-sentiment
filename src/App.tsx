2fe7a9169194b838d5ce747c49e0ef1e5a37c726
Desmond-Fon <desmondfon@yahoo.com>
2024-03-04 04:44:26 +0100

    added file upload for json files

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import './App.css'
-import { SideBar } from './components/SideBar'
-import { Main } from './components/Main'
-import { Nav } from './components/Nav'
-import { Footer } from './components/Footer'
-import { useState, useEffect } from 'react'
-import { Button } from "@chakra-ui/react";
+import "./App.css";
+import { SideBar } from "./components/SideBar";
+import { Main } from "./components/Main";
+import { Nav } from "./components/Nav";
+import { Footer } from "./components/Footer";
+import { useState, useEffect } from "react";
+import { Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
 import { MdProductionQuantityLimits } from "react-icons/md";
 
-// import config from './config.json';
+import config from "./config.json";
 
 export interface Product {
-  id: string,
+  id: string;
   image: string;
   name: string;
   last_comment_date: Date;
@@ -25,15 +25,14 @@ interface DashboardData {
 
 function App() {
   const [resData, setResData] = useState<DashboardData>();
-  const [show, setShow] = useState<boolean>(false)
-  const [link, setLink] = useState("");
+  const [show, setShow] = useState<boolean>(false);
+  const [link, setLink] = useState<File | null>(null);
   const [err, setErr] = useState(false);
-  const [linkErr, setLinkErr] = useState(false);
 
   useEffect(() => {
-    fetch(link)
-      .then(res => res.json() as Promise<DashboardData>)
-      .then(data => {
+    fetch(config.server_url)
+      .then((res) => res.json() as Promise<DashboardData>)
+      .then((data) => {
         setResData(data);
       })
       .catch((error) => {
@@ -41,16 +40,24 @@ function App() {
       });
   }, [setResData]);
 
-    const handleSubmit = () => {
-      if (link.length < 1) {
-        setErr(true);
-      } else if (link.startsWith("http://") || link.startsWith("https://")) {
-        setShow(true);
-      } else {
-        setLinkErr(true)
-      }
-    };
-  
+  const handleSubmit = () => {
+    if (!link && link === null) {
+      setErr(true);
+    } else {
+      setShow(true);
+    }
+  };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const firstFile = e.target?.files?.item(0);
+    if (firstFile) {
+      setLink(firstFile);
+    } else {
+      setErr(true)
+      setLink(null);
+    }
+  };
+
   return (
     <>
       {show ? (
@@ -83,52 +90,29 @@ function App() {
           </p>{" "}
           <form className="flex justify-center lg:items-center items-start mx-[40px] flex-col gap-6 text-textAsh mt-[20px] w-[80%] lg:w-[50%]">
             <div className="pb-4 w-full text-black font-[700]">
-              <label className="text-white">Please input DB link</label>
-              <input
-                type="text"
-                placeholder="Database link"
-                value={link}
-                onChange={(e) => setLink(e.target.value)}
-                className="w-[100%] border-[#808080] border-[1px] py-2 px-2 outline-0 rounded-[5px]"
-              />
-              {err && link === "" ? (
-                <p className="pt-2 text-red-400 text-[12px]">
-                  Input cannot be empty
-                </p>
-              ) : (
-                ""
-              )}
-              {linkErr && (!link.startsWith("http://") ||
-              !link.startsWith("https://")) ? (
-                <p className="pt-2 text-red-400 text-[12px]">
-                  Please enter a valid link
-                </p>
-              ) : (
-                ""
-              )}
+              <FormControl>
+                <FormLabel fontWeight={"700"} fontSize={"16px"} color='white'>
+                  Enter JSON FIle
+                </FormLabel>
+                <Input
+                  size="md"
+                  type={"file"}
+                  bg={"white"}
+                  onChange={handleFileChange}
+                  fontWeight={"400"}
+                  rounded={"xl"}
+                  fontSize={"15px"}
+                  outline={"none"}
+                  border={"none"}
+                  accept=".json"
+                />
+                {err && link === null ? <p className='text-red-400'>Please enter a json file</p> : ""}
+              </FormControl>
             </div>
-            {/* <div className="pb-4 w-full">
-            <label>
-              Password
-            </label>
-            <input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-[100%] border-[#808080] border-[1px] py-2 px-2 outline-0 rounded-[5px]"
-            />
-            {err && password === "" ? (
-              <p className="pt-2 text-red text-[12px]">Please enter password</p>
-            ) : (
-              ""
-            )}
-          </div> */}
             <Button
               className={`w-full py-2 px-6 mt-2 md:mt-0 md:px-9 bg-red text-white rounded-md`}
               bg="#239463"
               onClick={handleSubmit}
-              // isLoading={mutation.isPending}
             >
               SUBMIT
             </Button>
@@ -139,4 +123,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
