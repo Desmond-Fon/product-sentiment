9ae47048fd45c7197f52d900b9760220f273670c
toluwablaq <toluwablaq>
2024-03-06 23:01:45 +0100

    p _effect

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,9 @@ export const Footer = () => {
         <div className='bg-primary text-footerText py-4 flex justify-between items-center px-4 lg:px-8 text-[13px] w-full lg:text-[16px]'>
             <p>copyright prodSent</p>
             <div className='flex justify-between items-center gap-5'>
-                <p className='cursor-pointer hover:text-offWhite'>how it works</p>
-                <p className='cursor-pointer hover:text-offWhite'>privacy policy</p>
-                <p className='cursor-pointer hover:text-offWhite'>legal</p>
+                <p className='cursor-pointer hover:text-offWhite'></p>
+                <p className='cursor-pointer hover:text-offWhite'></p>
+                <p className='cursor-pointer hover:text-offWhite'></p>
             </div>
         </div>
     )
